1163d566bf827c787d406f69b23bbb26af189fcb
Bond, James Bond <no need of email>
2021-07-09 23:35:43 +0530

    ft: convert to typescript

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
 require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -17,7 +18,7 @@ task("accounts", "Prints the list of accounts", async () => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
+export default {
   networks: {
     hardhat: {
       allowUnlimitedContractSize: true,
