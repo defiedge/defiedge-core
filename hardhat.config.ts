a050d9ad84d18c340964d42a71ba487b0e8ad7e8
Sherlock <88290185+sherlauk@users.noreply.github.com>
2021-09-21 12:24:14 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,11 +29,11 @@ export default {
       gas: 12000000,
       blockGasLimit: 0x1fffffffffffff,
     },
-    kovan: {
-      url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-      gasLimit: 10000000000,
-    },
+    // kovan: {
+    //  url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY}`,
+    //  accounts: [process.env.PRIVATE_KEY],
+    // gasLimit: 10000000000,
+    // },
     //     mainnet: {
     //       url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
     //       accounts: [process.env.MAINNET_PRIVATE_KEY],
