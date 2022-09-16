db4cf5a55f8be0e4e6409e0cbad02e0e72243bc6
sherlauk <v@unbound.finance>
2022-09-16 22:27:58 +0530

    chore: add natspac plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,28 +1,29 @@
-import { task } from "hardhat/config";
-import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-import "@nomiclabs/hardhat-ganache";
-require("dotenv").config();
-import "hardhat-contract-sizer";
+import { task } from "hardhat/config"
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-ganache"
+require("dotenv").config()
+import "hardhat-contract-sizer"
+import "hardhat-docgen"
 
-const INFURA_KEY = process.env.INFURA_KEY || "1"; // for coverage
+const INFURA_KEY = process.env.INFURA_KEY || "1" // for coverage
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  "0xa3cf71bf26325d0175cd9d475141f9a46c29787d281707d78cffe11b1629b428"; // for coverage
+  "0xa3cf71bf26325d0175cd9d475141f9a46c29787d281707d78cffe11b1629b428" // for coverage
 const MAINNET_PRIVATE_KEY =
   process.env.MAINNET_PRIVATE_KEY ||
-  "0xa3cf71bf26325d0175cd9d475141f9a46c29787d281707d78cffe11b1629b428"; // for coverage
+  "0xa3cf71bf26325d0175cd9d475141f9a46c29787d281707d78cffe11b1629b428" // for coverage
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+  const accounts = await hre.ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -50,6 +51,11 @@ export default {
       gasPrice: 65000000000,
     },
   },
+  docgen: {
+    path: "./docs",
+    clear: true,
+    runOnCompile: true,
+  },
   solidity: {
     version: "0.7.6",
     settings: {
@@ -62,4 +68,4 @@ export default {
       },
     },
   },
-};
+}
